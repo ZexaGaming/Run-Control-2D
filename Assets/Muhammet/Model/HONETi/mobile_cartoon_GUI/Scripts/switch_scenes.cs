f8cb89d8862edf872ec2df4ee5724e8a5fc65b4e
MeyCode1910 <muhammeteminyorulmaz5@gmail.com>
2024-06-05 14:46:18 +0300

    EfektBitti

--- Assets/Muhammet/Model/HONETi/mobile_cartoon_GUI/Scripts/switch_scenes.cs
+++ Assets/Muhammet/Model/HONETi/mobile_cartoon_GUI/Scripts/switch_scenes.cs
@@ -6,12 +6,13 @@ public class switch_scenes : MonoBehaviour {
 	
 	public string sceneName = "";
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    [System.Obsolete]
+    void Start () {
 		Button b = GetComponent<Button> ();
 		if (b != null && sceneName != "")
 		{
-			b.onClick.AddListener(() => {Application.LoadLevel(sceneName);});
+			b.onClick.AddListener(() => { Application.LoadLevel(name: sceneName);});
 		}
 	}
 	
